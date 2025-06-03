29d2eef7a99085773c7e245e3dbed55e7430ce44
Flavius <flaviuscdinu93@gmail.com>
2025-06-03 10:02:56 +0300

    Added initial version

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ module "spacelift_datadog" {
   version = "0.2.5"
 
   dd_api_key   = var.dd_api_key
-  dd_site      = "datadoghq.eu"
-  space_id     = "root"
+  dd_site      = var.dd_site
+  space_id     = var.space_id
   extra_tags   = { "env" : "prod" }
   exclude_tags = ["run_note", "run_url"]
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
 variable "dd_api_key" {
   type = string
 }
+
+variable "dd_site" {
+  type    = string
+  default = "datadoghq.eu"
+}
+
+variable "space_id" {
+  type    = string
+  default = "root"
+}
